ed6e0ed3d86279a92aece5436cf8829e40445cd4
MouadhBouneb <102998690+MouadhBouneb@users.noreply.github.com>
2025-09-02 14:24:03 +0100

    Updated package.json

--- src/domain/value-objects/password.ts
+++ src/domain/value-objects/password.ts
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 
 export class Password {
   private constructor(private readonly value: string) {}
--- src/infrastructure/database/user.model.ts
+++ src/infrastructure/database/user.model.ts
@@ -1,5 +1,5 @@
 import mongoose, { Schema, Document, Model } from "mongoose";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import crypto from "crypto";
 
 export interface IUserDoc extends Document {
